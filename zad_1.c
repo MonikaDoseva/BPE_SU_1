11ee09f1bb247f1e530f74cd692bf6eae9ad065e
MonikaDoseva <mikazukimikazukova@gmail.com>
2026-04-23 08:36:48 +0300

    Create zad_1.c

---
+++ zad_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("edno\n");
+    printf("dve\n");
+    printf("tri\n");
+    printf("chetiri\n");
+    printf("pet\n");
+    printf("shest\n");
+    printf("sedem\n");
+    printf("osem\n");
+    printf("devet\n");
+    printf("deset\n");
+
+    return 0;
+}
